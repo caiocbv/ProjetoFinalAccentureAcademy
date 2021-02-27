26a4ec6d4cc68b05777bcc64e3d7604ca8437ec3
Guilherme Torato <guii.torato@gmail.com>
2021-02-26 22:59:08 -0300

    Subindo o projeto

---
+++ src/main/java/com/projeto/accenture/ProjetoFinalAccentureApplication.java
@@ -0,0 +1,21 @@
+package com.projeto.accenture;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalAccentureApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalAccentureApplication.class, args);
+		
+		
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
---
+++ src/main/java/com/projeto/accenture/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.projeto.accenture.config;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.models.auth.In;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	 @Bean
+	    public Docket api() { 
+	        return new Docket(DocumentationType.SWAGGER_2)  
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.any())              
+	          .paths(PathSelectors.any())                          
+	          .build();
+	          
+		        
+	    }
+	 
+
+}
+
