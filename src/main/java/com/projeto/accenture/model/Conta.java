60685d2db79b6940a421c6a16cc54a961a5c4224
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2021-02-27 15:16:15 -0300

    fazendo a correção da classe conta

--- src/main/java/com/projeto/accenture/model/Conta.java
+++ src/main/java/com/projeto/accenture/model/Conta.java
@@ -8,9 +8,50 @@ public class Conta {
 	private String loginUsuario;
 	private String numero;
 	private double saldo;
-	private TipoConta descricao;
-
+	//private TipoConta descricao;
 	
+	Conta(){
+		this.saldo=0;
+	}
+	
+	Conta(String loginUsuario){
+		this.loginUsuario=loginUsuario;
+		this.saldo=0;
+	}
+	
+	Conta(String loginUsuario, String numero){
+		this.loginUsuario=loginUsuario;
+		this.numero= numero;
+		this.saldo=0;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLoginUsuario() {
+		return loginUsuario;
+	}
+	public void setLoginUsuario(String loginUsuario) {
+		this.loginUsuario = loginUsuario;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+		
 	
 
 }
