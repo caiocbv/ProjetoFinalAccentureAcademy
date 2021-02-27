314c51d38bf9f44b7b47bdcf26705e6f2cd201b2
CaioBezerra <caiocbv@gmail.com>
2021-02-27 16:14:09 -0300

    Criação da classe UsuarioDTO

---
+++ src/main/java/com/projeto/accenture/model/UsuarioDTO.java
@@ -0,0 +1,46 @@
+package com.projeto.accenture.model;
+
+public class UsuarioDTO {
+	private String login;
+	private String nome;
+	private String senha;
+	private String cpf;
+	
+	public UsuarioDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
