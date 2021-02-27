6ef48962f707df9f8b30e92b15abf78368275415
Guilherme Torato <guii.torato@gmail.com>
2021-02-27 18:19:21 -0300

    Adc o SecurityConfig com as config do CORS e do Swagger

---
+++ src/main/java/com/projeto/accenture/config/SecurityConfig.java
@@ -0,0 +1,90 @@
+package com.projeto.accenture.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private Environment env;
+	
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+
+	};
+
+	private static final String[] PUBLIC_MATCHERS_GET = {		
+			
+	};
+
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/pacientes/**",
+			"/funcionarios/**",
+			"/tratamentos/**",
+			"/antibioticos/**",
+			"/hospital/**"
+	};
+	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+		
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.DELETE, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.PATCH, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {    
+	    web.ignoring().antMatchers("/v2/api-docs/**");
+	    web.ignoring().antMatchers("/swagger.json");
+	    web.ignoring().antMatchers("/swagger-ui.html");
+	    web.ignoring().antMatchers("/swagger-resources/**");
+	    web.ignoring().antMatchers("/webjars/**");
+	}
+	
+
+}
