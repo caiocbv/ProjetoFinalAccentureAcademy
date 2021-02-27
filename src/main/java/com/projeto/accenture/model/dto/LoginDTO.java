ede0ae48aa63012965233d088e2c198c6ef8a049
CaioBezerra <caiocbv@gmail.com>
2021-02-27 17:27:44 -0300

    Classe LoginDTO adicionada

---
+++ src/main/java/com/projeto/accenture/model/dto/LoginDTO.java
@@ -0,0 +1,28 @@
+package com.projeto.accenture.model.dto;
+
+public class LoginDTO {
+	private String login;
+	private String senha;
+	
+	public LoginDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
