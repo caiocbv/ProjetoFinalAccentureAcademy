f9a4d63d2fc4a2e6ce41e7513d9844b19fc51469
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2021-02-27 16:02:58 -0300

    Adicionando a classe referente ao planoDeConta

---
+++ src/main/java/com/projeto/accenture/model/PlanosDeConta.java
@@ -0,0 +1,33 @@
+package com.projeto.accenture.model;
+
+public class PlanosDeConta {
+	
+	private int id;
+	private String descricao;
+	private String login;
+	//private TipoMovimento tipoMovimento
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	@Override
+	public String toString() {
+		return "PlanosDeConta [id=" + id + ", descricao=" + descricao + ", login=" + login + "]";
+	}
+	
+	
+}
