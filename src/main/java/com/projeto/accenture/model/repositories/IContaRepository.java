375af8f655a76e5913047187e40f4be5da0b908d
CaioBezerra <caiocbv@gmail.com>
2021-02-28 17:09:35 -0300

    Criando IUsuarioService

--- src/main/java/com/projeto/accenture/model/repositories/IContaRepository.java
+++ src/main/java/com/projeto/accenture/model/repositories/IContaRepository.java
@@ -1,9 +1,12 @@
 package com.projeto.accenture.model.repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.projeto.accenture.model.Conta;
 
-public interface IContaRepository extends CrudRepository<Conta, Integer> {
 
+public interface IContaRepository extends CrudRepository<Conta, Integer> {
+	public ArrayList<Conta> findByLogin(String login);
 }
