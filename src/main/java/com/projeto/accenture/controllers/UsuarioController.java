e246f74580ec8a810e1e4f80ce3ab6f54d508a0f
Guilherme Torato <guii.torato@gmail.com>
2021-03-01 13:36:45 -0300

    Criado o método de busca por login ou por id

--- src/main/java/com/projeto/accenture/controllers/UsuarioController.java
+++ src/main/java/com/projeto/accenture/controllers/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.projeto.accenture.controllers;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projeto.accenture.dto.UsuarioDTO;
+import com.projeto.accenture.helpers.Helper;
 import com.projeto.accenture.model.Usuario;
 import com.projeto.accenture.services.IUsuarioService;
 
@@ -23,11 +28,22 @@ public class UsuarioController {
 	@Autowired
 	private IUsuarioService service;
 	
-	@ApiOperation(value = "BUSCAR POR ID DO USUÁRIO")
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UsuarioDTO> findId(@PathVariable Integer id){
+	// -------- MÉTODO DE LISTAR OS Usuarios, UTILIZEI UMA CLASSE DTO PARA FILTRAS AS INFORMAÇÕES PELO JSON
+		@RequestMapping(method = RequestMethod.GET)
+		public ResponseEntity<List<UsuarioDTO>> findAll(){
+			
+			List<Usuario> list = service.findAll();
+			List<UsuarioDTO> listDto = list.stream().map(obj -> new UsuarioDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
+		}
 		
-		Usuario obj = service.find(id);
+	
+	@ApiOperation(value = "BUSCAR POR LOGIN DO USUÁRIO OU POR ID")
+	@RequestMapping(value="/{param}", method = RequestMethod.GET)
+	public ResponseEntity<UsuarioDTO> findByLogin(@PathVariable String param){
+		
+		Usuario obj = null;
+		obj = Helper.isNumber(param) ? service.find(Integer.parseInt(param)) : service.findLogin(param);
 		UsuarioDTO objtDTO = new UsuarioDTO(obj);
 		
 		return ResponseEntity.ok().body(objtDTO);
@@ -35,7 +51,7 @@ public class UsuarioController {
 	
 	@ApiOperation(value = "CADASTRAR USUÁRIO")
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Usuario obj){
+	public ResponseEntity<Void> insert(@Validated @RequestBody Usuario obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").build(obj.getId());
