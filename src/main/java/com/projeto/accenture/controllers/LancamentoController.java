d8b17b15bc9826de847158b5caac2a94939cd6ff
Guilherme Torato <guii.torato@gmail.com>
2021-03-03 15:57:12 -0300

    Feito o endpoint para lista lancamento, lista e cadastro plano conta

--- src/main/java/com/projeto/accenture/controllers/LancamentoController.java
+++ src/main/java/com/projeto/accenture/controllers/LancamentoController.java
@@ -1,39 +1,75 @@
 package com.projeto.accenture.controllers;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projeto.accenture.dto.LancamentoDTO;
+import com.projeto.accenture.dto.PlanoContaDTO;
 import com.projeto.accenture.dto.UsuarioDTO;
-import com.projeto.accenture.services.ILancamentoService;
+import com.projeto.accenture.model.Lancamento;
+import com.projeto.accenture.model.PlanosDeConta;
+import com.projeto.accenture.model.Usuario;
+import com.projeto.accenture.services.LancamentoService;
+import com.projeto.accenture.services.PlanoContaService;
+import com.projeto.accenture.services.UsuarioService;
 
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value="/api/v1/lancamento")
 public class LancamentoController {
-	@Autowired
-	private ILancamentoService service;
 	
-	@ApiOperation(value = "Listar Lancamentos do Usuário")
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<List<LancamentoDTO>> findByUsuario(@Validated @RequestBody UsuarioDTO obj){
-		ResponseEntity<List<LancamentoDTO>> retorno = this.service.findByConta(obj.getLogin());
-		return retorno;
+	@Autowired
+	private LancamentoService serviceLancamento;
+	
+	@Autowired
+	private UsuarioService serviceUser;
+	
+	@Autowired
+	private PlanoContaService serivcePlano;
+
+// -------------------- MÉTODOS PLANOS DE CONTA ---------------------------------	
+	@ApiOperation(value = "CADASTRAR PLANO DE CONTA")
+	@RequestMapping(value="/planodeconta",method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Validated @RequestBody PlanosDeConta obj , @RequestParam String login){
+		
+		serivcePlano.insert(obj, login);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").build(obj.getId());
+		
+		return ResponseEntity.created(uri).build();	
+		
 	}
 	
-	@ApiOperation(value = "Listar Lancamentos do Usuário")
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<LancamentoDTO> registrarLancamento(@Validated @RequestBody LancamentoDTO obj){
-		ResponseEntity<LancamentoDTO> retorno = this.service.registrarLancamentoDTO(obj);
-		return retorno;
+	@ApiOperation(value = "LISTA DE TODOS OS PLANOS DE CONTA")
+	@RequestMapping(value="/planodeconta",method = RequestMethod.GET)
+	public ResponseEntity<List<PlanosDeConta>> findAll(){
+		
+		List<PlanosDeConta> list = serivcePlano.findAllPlano();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+// -------------------- MÉTODOS LANCAMENTOS---------------------------------	
+	@ApiOperation(value = "LISTA DE TODOS OS LANCAMENTOS")
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<LancamentoDTO>> findAllLancamento(@Validated @RequestParam String login){
+		
+		List<Lancamento> list = serviceLancamento.findAll(login);
+		List<LancamentoDTO> listDTO = list.stream().map(obj -> new LancamentoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 	
