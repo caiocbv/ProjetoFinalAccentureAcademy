7258fd23074c7538e1a40d6d260f61f3d4f40b2f
CaioBezerra <caiocbv@gmail.com>
2021-03-03 18:11:18 -0300

    Alteracoes Swagger

--- src/main/java/com/projeto/accenture/config/SwaggerConfig.java
+++ src/main/java/com/projeto/accenture/config/SwaggerConfig.java
@@ -22,6 +22,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -37,11 +39,25 @@ public class SwaggerConfig {
 	          .select()                                  
 	          .apis(RequestHandlerSelectors.any())              
 	          .paths(PathSelectors.any())                          
-	          .build();
+	          .build()
+	          .apiInfo(metaInfo());
 	          
 		        
 	    }
 	 
+	 private ApiInfo metaInfo() {
+		 ApiInfo apiInfo = new ApiInfo(
+				 "Behind The Lines API REST",
+				 "API REST Uma ferramenta APIRest utilizada no auxílio da gestão de contas e lançamentos financeiros de um usuário",
+				 "1.0",
+				 "Terms of Service",
+				 new Contact("Caio Bezerra", "https://github.com/caiocbv", "caiocbv@gmail.com"),
+				 "Apache License Version 2.0",
+				 "https://www.apache.org/license.html",
+				 new ArrayList<VendorExtension>()
+				 );
+		 return apiInfo;
+	 }
 
 }
 
